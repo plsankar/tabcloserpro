a7001283097da7c35c4ec58019eb365e2432c2fb
Lakshmi Sankar <plsankar1996@gmail.com>
2024-06-28 22:58:49 +0530

    feat: added shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,9 @@ export default defineConfig({
         },
     },
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
