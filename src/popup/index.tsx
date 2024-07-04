73d98122b9f30d60fa475475040b69ae25b514fb
Lakshmi Sankar <plsankar1996@gmail.com>
2024-07-04 21:50:00 +0530

    chore: removed unwanted dependencies

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,14 +4,12 @@ import App from "./app.tsx";
 import NavigationMenu from "./components/navigation-menu.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <div className="text-[0.825rem] antialiased h-full">
             <App />
-            <Toaster position="bottom-center" />
             <Tooltip id="app-tooltip" />
             <NavigationMenu />
         </div>
